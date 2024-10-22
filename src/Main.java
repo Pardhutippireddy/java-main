f0eb1b90c3ff3fc4cfceab9bcf8a4addfdcf5c17
Paardhu <ptippireddy001@gmail.com>
2024-10-21 20:41:32 -0400

    first code

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+//Given an integer, , perform the following conditional actions:
+//
+//        If n is odd, print Weird
+//        If n is even and in the inclusive range of 2 to 5 , print Not Weird
+//        If  is even and in the inclusive range of 6 to 20, print Weird
+//        If  is even and greater than 20, print Not Weird
+//        Complete the stub code provided in your editor to print whether or not  is weird.
+
+public class Main
+{
+    public static void main(String args[])
+    {
+
+        int n = 0;
+
+        if (n > 1)
+            System.out.println("Weird");
+        else
+            System.out.println("Not Weird");
+    }
+}
